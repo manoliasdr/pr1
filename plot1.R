43fecc284f2f33988ec6f9ecf66dbb496d34c089
manoliasdr <emma_sot@hotmail.gr>
2018-08-27 18:41:22 +0300

    Added all files

---
+++ plot1.R
@@ -0,0 +1,10 @@
+dataFile<-"C:/Documents and Settings/Σωτήρχος/Επιφάνεια εργασίας/coursera/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+head(data)
+
+global_active_power_1<-as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(global_active_power_1,col="darkred",main="Global Active Power", xlab="Global Active Power (kilowatts)")
+rug(global_active_power_1)
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,11 @@
+dataFile<-"C:/Documents and Settings/Σωτήρχος/Επιφάνεια εργασίας/coursera/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+head(data)
+
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+global_active_power_2 <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, global_active_power_2,type="l" ,xlab="", ylab="Global Active Power (kilowatts)")
+title(main="Plot 2")
+dev.off()
